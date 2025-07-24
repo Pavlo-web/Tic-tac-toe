ac6ae416dbce43496b539099e922e707a8a5812d
Pavlo P.O. Onyshko <pavlo.onyshko@dev.com.ua>
2025-07-24 11:29:52 +0300

    create rep

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,23 @@
+export default function GameBoard({ onSelectSquare, board }) {
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(colIndex, rowIndex)}
+                  disabled={playerSymbol !== null}
+                  className={playerSymbol}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
